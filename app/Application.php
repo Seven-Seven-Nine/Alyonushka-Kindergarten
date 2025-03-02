0def0b5ee72b669bacfb64f42dba785547ec2620
Seven-Seven-Nine <danila.umbetaliev.05@mail.ru>
2025-03-02 12:43:25 +0500

    Added ability to create statements

--- app/Application.php
+++ app/Application.php
@@ -9,9 +9,55 @@ class Application {
         if ($connect->connect_errno) {
             die('<p class="error-text">Ошибка подключения к базе данных!</p>');
         } else {
-            
+            if ($this->check_application($_POST['child-name'], $_POST['child-surname'])) {
+                $login = $_SESSION['login'];
+                $child_name = $_POST['child-name'];
+                $child_surname = $_POST['child-surname'];
+                $child_patronymic = $_POST['child-patronymic'];
+                $child_birthdate = $_POST['child-birthdate'];
+                $parent_name = $_POST['parent-name'];
+                $parent_surname = $_POST['parent-surname'];
+                $parent_patronymic = $_POST['parent-patronymic'];
+                $parent_phone = $_POST['parent-phone'];
+                $parent_email = $_POST['parent-email'];
+                $address = $_POST['address'];
+                $desired_group = $_POST['desired_group'];
+                $application_date = date('Y-m-d');
+    
+                $stmt = $connect->prepare('INSERT INTO `applications` (`login`, `child_name`, `child_surname`, `child_patronymic`, `child_birthdate`, `parent_name`, `parent_surname`, `parent_patronymic`, `parent_phone`, `parent_email`, `address`, `desired_group`, `application_date`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+    
+                $stmt->bind_param('sssssssssssss', $login, $child_name, $child_surname, $child_patronymic, $child_birthdate, $parent_name, $parent_surname, $parent_patronymic, $parent_phone, $parent_email, $address, $desired_group, $application_date);
+                $stmt->execute();
+    
+                $stmt->close();
+                $connect->close();
+    
+                header('Location: /static/index.php?page=applications&result_application=application_created');
+            } else {
+                header('Location: /static/index.php?page=applications&error_application=the_application_has_already_been_created');
+            }
+        }
+    }
 
-            $stmt = $connect->prepare('');
+    private function check_application(string $child_name, string $child_surname): bool {
+        $connect = connect_data_base();
+
+        if ($connect->connect_error) {
+            die('<p class="error-text">Ошибка подключения к базе данных!</p>');
+        } else {
+            $stmt = $connect->prepare('SELECT `login`, `child_name`, `child_surname` FROM `applications` WHERE `login` = ? AND `child_name` = ? AND `child_surname` = ?');
+            $stmt->bind_param('sss', $_SESSION['login'], $child_name, $child_surname);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            $stmt->close();
+            $connect->close();
+
+            if ($result->num_rows > 0) {
+                return false;
+            } else {
+                return true;
+            }
         }
     }
 }
--- static/modules/applications.php
+++ static/modules/applications.php
@@ -1,17 +1,17 @@
 <?php
-    // require_once '../';
+    require_once '../app/Application.php';
 
     if (!isset($_SESSION['login'])) {
         header('Location: /static/index.php?page=main');        
     }
 
-    // $application = 
+    $application = new Application();
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (isset($_GET['mode'])) {
             switch ($_GET['mode']) {
                 case 'create_application':
-                    # code...
+                    $application->create_application_user();
                     break;
                 default:
                     echo '<p class="error-text">Неизвестный режим выполнения.</p>';
@@ -26,6 +26,28 @@
         <a class="arrow-back" href="/static/index.php?page=account">←</a>
     </div>
     <?php
+        if (isset(($_GET['result_application']))) {
+            switch ($_GET['result_application']) {
+                case 'application_created':
+                    echo '<p class="result-green-text">Заявление создано!</p>';
+                    break;
+                default:
+                    echo '<p class="error-text">Неизвестный результат заявления.</p>';
+                    break;
+            }
+        }
+
+        if (isset(($_GET['error_application']))) {
+            switch ($_GET['error_application']) {
+                case 'the_application_has_already_been_created':
+                    echo '<p class="error-text">Заявление уже создано.</p>';
+                    break;
+                default:
+                    echo '<p class="error-text">Неизвестный результат заявления.</p>';
+                    break;
+            }
+        }
+
         if ($_SESSION['login'] == 'administrator') {
             echo '<p>Заявления для админа.</p>';
         } else {
