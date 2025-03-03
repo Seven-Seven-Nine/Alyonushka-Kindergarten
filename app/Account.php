d26f3715cbb145178fdebfa47f2fd7fcaea9420c
Seven-Seven-Nine <danila.umbetaliev.05@mail.ru>
2025-03-03 22:42:08 +0500

    Added the ability to view and delete users for the admin

--- app/Account.php
+++ app/Account.php
@@ -40,4 +40,117 @@ class Account {
             $connect->close();
         }
     }
+
+    public function get_all_list_users(): void {
+        $connect = connect_data_base();
+
+        if ($connect->connect_errno) {
+            die('<p class="error-text">Ошибка подключения к базе данных!</p>');
+        } else {
+            $stmt = $connect->prepare('SELECT `id`, `login`, `role`, `email` FROM `users`');
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            $stmt->close();
+            $connect->close();
+
+            while ($row = $result->fetch_assoc()) {
+                if ($row['role'] != 'administrator') {
+                    echo '
+                        <a title="Просмотреть|Удалить|Изменить пользователя №'. $row['id'] .'" class="application-button" href="/static/index.php?page=admin_users&id_users='. $row['id'] .'">'. $row['login'] .' | '. $row['role'] .' | '. $row['email'] .'</a>
+                    ';
+                }
+            }
+        }
+    }
+
+    public function get_info_user_by_id(string $id_user): void {
+        $connect = connect_data_base();
+
+        if ($connect->connect_errno) {
+            die('<p class="error-text">Ошибка подключения к базе данных!</p>');
+        } else {
+            $stmt = $connect->prepare('SELECT `id`, `login`, `role`, `email` FROM `users` WHERE `id` = ?');
+            $stmt->bind_param('s', $id_user);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            $stmt->close();
+            $connect->close();
+
+            while ($row = $result->fetch_assoc()) {
+                if ($row['role'] != 'administrator') {
+                    echo '
+                        <h2>Пользователь: '. $row['login'] .' | Роль: '. $row['role'] .'</h2>
+                        <hr>
+                        <p>Почта: '. $row['email'] .'</p>
+                    ';
+                    $this->get_all_application_user_by_login($row['login']);
+                }
+            }
+        }
+    }
+
+    private function get_all_application_user_by_login(string $login): void {
+        $connect = connect_data_base();
+
+        if ($connect->connect_errno) {
+            die('<p class="error-text">Ошибка подключения к базе данных!</p>');
+        } else {
+            $stmt = $connect->prepare('SELECT `id`, `child_name`, `child_surname`, `child_patronymic`, `application_date`, `status` FROM `applications` WHERE `login` = ?');
+            $stmt->bind_param('s', $login);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            
+            $stmt->close();
+            $connect->close();
+
+            while ($row = $result->fetch_assoc()) {
+                echo '
+                    <a class="application-button" title="Рассмотреть заявление №'. $row['id'] .'" href="/static/index.php?page=applications&consider_the_application='. $row['id'] .'">'. $row['child_name'] .' '. $row['child_surname'] .' '. $row['child_patronymic'] .' | Дата: '. $row['application_date'] .' | Статус: '. $row['status'] .'</a>
+                ';
+            }
+        }
+    }
+
+    public function delete_user_by_id(string $id_user): void {
+        $connect = connect_data_base();
+
+        if ($connect->connect_errno) {
+            die('<p class="error-text">Ошибка подключения к базе данных!</p>');
+        } else {
+            $stmt = $connect->prepare('DELETE FROM `users` WHERE `id` = ?');
+            $stmt->bind_param('s', $id_user);
+            $stmt->execute();
+
+            $stmt->close();
+            $connect->close();
+
+            $this->delete_applications_user_by_id($id_user);
+
+            header('Location: /static/index.php?page=admin_users&result_users=user_deleted');
+        }
+    }
+
+    private function delete_applications_user_by_id(string $id_user): void {
+        $connect = connect_data_base();
+
+        if ($connect->connect_errno) {
+            die('<p class="error-text">Ошибка подключения к базе данных!</p>');
+        } else {
+            $stmt = $connect->prepare('SELECT `login` FROM `users` WHERE `id` = ?');
+            $stmt->bind_param('s', $id_user);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            while ($row = $result->fetch_assoc()) {
+                $stmt->prepare('DELETE FROM `applications` WHERE `login` = ?');
+                $stmt->bind_param('s', $row['login']);
+                $stmt->execute();
+            }
+
+            $stmt->close();
+            $connect->close();
+        }
+    }
 }
--- static/modules/admin_users.php
+++ static/modules/admin_users.php
@@ -1,12 +1,67 @@
 <?php
+    require_once '../app/Account.php';
+
     if ($_SESSION['role'] != 'administrator') {
         header('Location: /static/index.php?page=main');        
     }
+
+    $account = new Account();
 ?>
 
-<main class="flex-start-column-center">
-    <div class="block-arrow-back" flex-row-center">
-        <a class="arrow-back" href="/static/index.php?page=account">←</a>
-    </div>
-    <p>Тута все пользователи и т.д.</p>
+<main class="account-page flex-start-column-center">
+    <?php
+        if (isset($_GET['id_users'])) {
+            echo '
+                <div class="block-arrow-back" flex-row-center">
+                    <a class="arrow-back" href="/static/index.php?page=admin_users">←</a>
+                </div>      
+            ';
+        } else {
+            echo '
+                <div class="block-arrow-back" flex-row-center">
+                    <a class="arrow-back" href="/static/index.php?page=account">←</a>
+                </div>      
+            ';
+        }
+
+        if (isset($_GET['result_users'])) {
+            switch ($_GET['result_users']) {
+                case 'user_deleted':
+                    echo '<p class="result-red-text">Пользователь удалён.</p>';
+                    break;
+                default:
+                    echo '<p class="error-text">Неизвестный результат.</p>';
+                    break;
+            }
+        }
+    ?>
+    <?php
+        if (isset($_GET['id_users'])) {
+            if (isset($_GET['action_admin_users'])) {
+                switch ($_GET['action_admin_users']) {
+                    case 'change_user':
+                        break;
+                    case 'delete_user':
+                        $account->delete_user_by_id($_GET['id_users']);
+                        break;
+                    default:
+                        echo '<p class="error-text">Неизвестное действие над пользователем! Изверг!</p>';
+                        break;
+                }
+            } else {
+                echo '
+                    <div class="flex-row-center panel">
+                        <a href="/static/index.php?page=admin_users&id_users='. $_GET['id_users'] .'&action_admin_users=delete_user" class="button-panel button-delete">Удалить</a>
+                    </div>
+                ';
+                echo '<div class="flex-column-center panel list-users">';
+                $account->get_info_user_by_id($_GET['id_users']);
+                echo '</div>';
+            }
+        } else {
+            echo '<div class="flex-column-center panel list-users">';
+            $account->get_all_list_users();
+            echo '</div>';
+        }
+    ?>
 </main>
